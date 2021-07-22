4575fde061d265fb45ab6a19a281e06f60ff80a6
shankar-auto <87353194+shankar-auto@users.noreply.github.com>
2021-07-22 12:36:46 +0530

    Update greetUser.groovy
    greet user file added

--- vars/greetUser.groovy
+++ vars/greetUser.groovy
@@ -1,1 +1,16 @@
-
+def call(string username = 'null') {
+	echo "this is share library greetUser: $(username)
+	pipeline {
+		environment {
+			USER_NAME = "$username"
+		}
+		agent any
+			stages {
+				stage {
+					steps {
+						echo "HI $USER_NAME, How are you !"
+					}
+				}
+			}
+		}
+	}
